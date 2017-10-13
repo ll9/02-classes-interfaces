46361a32deb0f11e8f96c02e33be242185ff8e09
ll9 <llwierer@web.de>
2017-10-13 15:51:49 +0200

    added nested Element class

--- src/main/java/de/fhro/inf/prg3/a02/SimpleListImpl.java
+++ src/main/java/de/fhro/inf/prg3/a02/SimpleListImpl.java
@@ -5,7 +5,10 @@ package de.fhro.inf.prg3.a02;
  * Created on 10/6/17.
  */
 public class SimpleListImpl implements SimpleList {
+    Element head;
 
-	// TODO: Implement the required methods.
-
+    private static class Element {
+        private Object item;
+        private Element next;
+    }
 }
