c484430582fdd3353e2445dd96aef722f1105bc8
ll9 <llwierer@web.de>
2017-10-13 16:09:16 +0200

    implemented Iterator as inner class

--- src/main/java/de/fhro/inf/prg3/a02/SimpleListImpl.java
+++ src/main/java/de/fhro/inf/prg3/a02/SimpleListImpl.java
@@ -1,14 +1,46 @@
 package de.fhro.inf.prg3.a02;
 
+import java.util.Iterator;
+
 /**
  * @author Peter Kurfer
  * Created on 10/6/17.
  */
 public class SimpleListImpl implements SimpleList {
-    Element head;
+    Element head = null;
 
     private static class Element {
-        private Object item;
-        private Element next;
+        private Object item = null;
+        private Element next = null;
+
+        public Object getItem() {
+            return item;
+        }
+
+        public Element getNext() {
+            return next;
+        }
+
+        public void setNext(Element next) {
+            this.next = next;
+        }
+    }
+
+    private class MyIterator implements Iterator<Element> {
+        Element cursor = head;
+
+        @Override
+        public boolean hasNext() {
+            return cursor != null;
+        }
+
+        @Override
+        public Element next() {
+            if (this.hasNext()) {
+                Element current = cursor;
+                cursor = cursor.getNext();
+                return current;
+            }
+        }
     }
 }
